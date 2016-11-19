158d387eeb67074e61ac4ca07a14b34493ac1476
Dillon <merrittd42@gmail.com>
2016-11-19 15:55:38 -0500

    cool

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-=======
+
   get 'bars', to: 'bar#index'
 
->>>>>>> 3d0e9909572196c4a9267c0c96794efcfc2226ea
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/bars/', to: 'bar#index'
+
 end
