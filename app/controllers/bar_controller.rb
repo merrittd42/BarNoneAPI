3d0e9909572196c4a9267c0c96794efcfc2226ea
frank <Frank Dattalo>
2016-11-19 15:49:25 -0500

    added get route

--- app/controllers/bar_controller.rb
+++ app/controllers/bar_controller.rb
@@ -1,11 +1,19 @@
 class BarController < ApplicationController
   def index
-    cords = latitude_and_longitude
+    latitude = latitude_and_longitude[0].to_f
+    longitude = latitude_and_longitude[1].to_f
+
+    render json: {
+      bars: Bar.where(
+        latitude: [latitude - 1, latitude + 1],
+        longitude: [longitude - 1, longitude + 1]
+        )
+    }
   end
 
   private
 
   def latitude_and_longitude
-    params.require(:latitude, :longitude)
+    params.require([:latitude, :longitude])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'bar/index', as: 'bars'
+  get 'bars', to: 'bar#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
