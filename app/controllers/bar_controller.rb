93cae0e4a3da71ae949fc4c4514f91172d3fc925
Dillon <merrittd42@gmail.com>
2016-11-19 22:01:36 -0500

    implemented attribute updates in bar controller

--- app/controllers/bar_controller.rb
+++ app/controllers/bar_controller.rb
@@ -19,6 +19,61 @@ class BarController < ApplicationController
     end
   end
 
+  def updateReview
+
+    begin
+
+      barRating = params[:rating].to_i
+      barCrowdedness = params[:crowdedness].to_i
+      barLoudness = params[:loudness].to_i
+      barID = params[:id].to_i
+
+      barToChange = Bar.find(barID)
+      newCrowdedness = barToChange.crowdedness + barCrowdedness
+      barToChange.crowdedness_count +=1
+      barToChange.crowdedness = newCrowdedness/barToChange.crowdedness_count
+      newRating = barRating + barToChange.rating
+      barToChange.rating_count+=1
+      barToChange.rating = newRating/barToChange.rating_count
+      barToChange.loudness = barLoudness
+      newLoudness = barToChange.loudness + barLoudness
+      barToChange.loudness_count +=1
+      barToChange.loudness = newLoudness/barToChange.loudness_count
+
+    rescue
+      print 'ERROR'
+      puts e
+      render json: { bars: []}
+    end
+  end
+
+  def updateCount
+ #0.1 miles is 0.001140 lat/long units
+    begin
+
+      latitude = params[:latitude].to_d
+      longitude = params[:longitude].to_d
+
+      bars = Bar.where(
+      "latitude >= :latitudeLow AND latitude <= :latitudeHigh AND longitude >= :longitudeLow AND longitude <= :longitudeHigh",
+      {latitudeLow: latitude - 0.00057, latitudeHigh: latitude + 0.00057, longitudeLow: longitude - 0.00057, longitudeHigh: longitude + 0.00057}
+
+      )
+
+
+      bars.each do |t|
+        t.person_count  += 1
+        t.save
+      end
+
+    rescue
+      print 'ERROR'
+      puts e
+      render json: { bars: []}
+    end
+  end
+
+
   private
 
   def latitude_and_longitude
--- config/routes.rb
+++ config/routes.rb
@@ -15,4 +15,8 @@ Rails.application.routes.draw do
 
   get 'bars', to: 'bar#index'
 
+  post 'bars/review/:id', controller: :bar, action: :updateReview
+
+  post 'bars/:id', controller: :bar, action: :updateCount
+
 end
